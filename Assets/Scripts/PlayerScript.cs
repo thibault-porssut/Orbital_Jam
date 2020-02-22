5db65929f4506eb103bd530b1d3bbde9f7363867
Kilian Ruchti <kilian.ruchti@gmail.com>
2020-02-22 19:53:07 +0100

    Player
    Player movement + light + food and energy collecting

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float moveSpeed;
+    public int energy = 0;
+    public int food = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveSpeed = 5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Move(moveSpeed);
+
+    }
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 100, 20), "Food : " + food);
+        GUI.Label(new Rect(10, 30, 100, 20), "Energy : " + energy);
+    }
+
+    private void Move(float Speed)
+    {
+        //transform.Translate(Speed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, 0f); //Movement only horizontally
+        transform.Translate(Speed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, Speed * Input.GetAxis("Vertical") * Time.deltaTime);
+    }
+}
